0a6617dfb028d7f405490b3745844994336ec5e7
Rodrigolmg <rodrigo.leitemg@gmail.com>
2022-08-26 00:06:43 -0300

    Transitions css classes defined

---
+++ animacoes-exercicios/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="show = !show">Show Message</b-button>
+		<transition name="fade">
+			<b-alert variant="info" show v-if="show">{{msg}}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data(){
+		return {
+			msg: 'A new information message for users',
+			show: false
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+
+.fade-enter {
+
+}
+
+.fade-enter-active {
+
+}
+
+.fade-enter-to {
+
+}
+
+.fade-leave {
+
+}
+
+.fade-leave-active {
+
+}
+
+.fade-leave-to {
+
+}
+</style>
